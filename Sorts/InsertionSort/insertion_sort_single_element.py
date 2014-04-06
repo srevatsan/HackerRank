7c82605ff7e85595ab896c91897027225fc50a4c
Sri Muralidharan <srevatsan@edmodo.com>
2014-04-06 14:45:05 -0700

    Insertion Sort Single element add

--- Sorts/InsertionSort/insertion_sort_single_element.py
+++ Sorts/InsertionSort/insertion_sort_single_element.py
@@ -1,22 +1,37 @@
 __author__ = 'smuralid'
 import sys
+__author__ = 'smuralid'
+import sys
 
+def insertion_sort(ar,s):
+    v = ar[s-1]
+    j=0
+    for i in range(1,s):
+      if ar[(s-1-i)] > v:
+        ar[(s-1-i)+1] = ar[(s-1-i)]
+      else:
+        ar[(s-1-i)+1] = v
+        return ar
+        j = 1
+        break
+    if j == 0:
+      ar[0] = v
+      return ar
+
+#Fetch the input list array length
 s = int(sys.stdin.readline().strip())
+
+#Fetch the list elements that needs to be sorted.
 ar = [int(elem) for elem in sys.stdin.readline().split()]
-v = ar[s-1]
-j=0
-for i in range(1,s):
-  if ar[(s-1-i)] > v:
-    ar[(s-1-i)+1] = ar[(s-1-i)]
-    print (str(ar).strip('[]').replace(',',''))
-  else:
-    ar[(s-1-i)+1] = v
-    print (str(ar).strip('[]').replace(',',''))
-    j = 1
-    break
-if j == 0:
-  ar[0] = v
-  print (str(ar).strip('[]').replace(',',''))
+
+#Initiatlizing sorted array
+sorted_array = []
+
+for i in range(2,s+1):
+  print (str(insertion_sort(ar,i)).strip('[]').replace(',',''))
+
+
+
 
 
 
