53a0dab61bc28a4f43b739dff7bbbece6ea4720c
Sri Muralidharan <srevatsan@edmodo.com>
2014-04-16 06:32:41 -0700

    QuickSort

--- Sorts/QuickSort/quick_sort.py
+++ Sorts/QuickSort/quick_sort.py
@@ -5,46 +5,56 @@ import sys
 # ar = [int(elem) for elem in sys.stdin.readline().split()]
 
 n = 7
-ar = [5, 8, 1, 3, 7, 9, 2]
+arr = [2,5,3,109]
 
 
 def quick_sort(ar):
-    # N is the length of the array
     n = len(ar)
-    if n==1:
-        print "printing",ar
+    if n == 1:
+        print (ar)
         return ar
-    ar_left = []
-    ar_right = []
-    p = ar[0]
 
-    for i in range(1,n):
-        if  ar[i]>p:
-            ar_right.append(ar[i])
-        else:
-            ar_left.append(ar[i])
-
-
-
-    n_left = len(ar_left)
-    n_right = len(ar_right)
-
-    print n_right,n_left
-
-    if n_left !=0:
-        print "Here0", ar,ar_left,quick_sort(ar_left),n_left
-        ar = quick_sort(ar_left).append(p)
-        print "Here1", ar,ar_left,quick_sort(ar_left),n_left
     else:
-        ar = [p]
+        p = ar[0]
+        ar_left = []
+        ar_right = []
+        for i in range(1,n):
+            if ar[i]>p:
+                ar_right.append(ar[i])
+            else:
+                ar_left.append(ar[i])
+            n_left = len(ar_left)
+            n_right = len(ar_right)
 
-    if n_right !=0:
-        ar_right = quick_sort(ar_right)
-        ar = ar + ar_right
+            if n_left > 0:
+                ar_left_swap = quick_sort(ar_left)
+            else:
+                ar_left_swap = []
 
+            if n_right > 0:
+                ar_right_swap = quick_sort(ar_right)
+            else:
+                ar_right_swap = []
+        if ar_left_swap:
+            ar = ar_left_swap
+        else:
+            ar = []
+        ar = ar + [p]
 
+        if ar_right_swap:
+            ar = ar + ar_right_swap
     return ar
-print quick_sort(ar)
+
+print (quick_sort(arr))
+
+
+
+
+
+
+
+
+
 
 
 
